20f3146307eb145a2ddc30f4a775c6da9dbc6994
César Almeida <cesar.almeida@vocedm.com.br>
2023-06-01 06:41:54 -0300

    first commit

---
+++ projetos/dart_fundamentos_vscode/bin/dart_fundamentos_vscode.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fundamentos_vscode/dart_fundamentos_vscode.dart' as dart_fundamentos_vscode;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fundamentos_vscode.calculate()}!');
+}
---
+++ projetos/dart_fundamentos_vscode/test/dart_fundamentos_vscode_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fundamentos_vscode/dart_fundamentos_vscode.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
