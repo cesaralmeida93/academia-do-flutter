a68ceb53b9ad4aaa60fc4452670b03469e66589b
César Almeida <cesar.almeida@vocedm.com.br>
2024-06-01 20:29:06 -0300

    created CompareTo impl

--- projetos/02_dart_funcoes_colecoes/lib/2_listas/1_listas.dart
+++ projetos/02_dart_funcoes_colecoes/lib/2_listas/1_listas.dart
@@ -46,11 +46,62 @@ void main() {
   listaParaOrdenacao.sort();
   print(listaNomesrdenacao);
 
-  var listaPaciente = [
-    'Rodrigo Rahman|30|Especialista Dart e FLutter|SP',
-    'Luana Rahman|30|Analista|SP',
-    'Guilherme Rahman|30|Analista|SP',
+  var listaPacientes = [
+    'Rodrigo Rahman|37',
+    'Luana Rahman|35',
+    'Guilherme|18',
+    'Arthur|5',
+    'Joaquim|5',
+    'Antoniol|50',
   ];
+
+  var novaListaPacientes = [...listaPacientes];
+
+  novaListaPacientes.sort((paciente1, paciente2) {
+    final pacienteDados1 = paciente1.split('|');
+    final pacienteDados2 = paciente2.split('|');
+
+    final idadePaciente1 = int.parse(pacienteDados1[1]);
+    final idadePaciente2 = int.parse(pacienteDados2[1]);
+
+    print("pacienteDados1: $pacienteDados1");
+    print("pacienteDados2: $pacienteDados2");
+    print("idadePaciente1: $idadePaciente1");
+    print("idadePaciente2: $idadePaciente2");
+
+    if (idadePaciente1 > idadePaciente2) {
+      return 1;
+    } else if (idadePaciente1 == idadePaciente2) {
+      return 0;
+    } else {
+      return -1;
+    }
+  });
+
+  print(listaPacientes);
+  print(novaListaPacientes);
+
+  // CompareTo
+  print('.sort com CompareTo');
+  var listaPacientes2 = [
+    'Rodrigo Rahman|37',
+    'Luana Rahman|35',
+    'Guilherme|18',
+    'Arthur|5',
+    'Joaquim|5',
+    'Antoniol|50',
+  ];
+
+  listaPacientes2.sort((paciente1, paciente2) {
+    final pacienteDados1 = paciente1.split('|');
+    final pacienteDados2 = paciente2.split('|');
+
+    final idadePaciente1 = int.parse(pacienteDados1[1]);
+    final idadePaciente2 = int.parse(pacienteDados2[1]);
+
+    return idadePaciente1.compareTo(idadePaciente2);
+  });
+  print(listaPacientes2);
 }
 
 void printNumeros(int valor) {
